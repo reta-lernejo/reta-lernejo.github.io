827b494deba03a5e92d596422446c4705ff28bd9
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-06-20 16:31:59 +0200

    plibonigo mapo fosforciklo kaj navigado

--- assets/js/yedmap-0a.js
+++ assets/js/yedmap-0a.js
@@ -43,32 +43,45 @@ class YedMap {
             const id = n.id;
             if (id.startsWith('y.node')) {
                 // ni ekstraktas url + text
-                let url = '', text = [], x = 0, y = 0, w = 0;
+                let url = '', text = [], x = 0, y = 0, w = 0, h = 0;
                 // eltrovu la url en elmento a - per ĝi ni alternative identigos la nodojn
                 const a = n.querySelector("a");
                 if (a) {
                     url = a.getAttribute("xlink:href");
+                    // forigu target-atributojn montrantaj al alia fenestro!
+                    a.removeAttribute("target");
+                    a.removeAttributeNS(xlink,"show");
                 }
                 // eltrovu la pozicion de la unua text-elemento
                 const t = n.querySelector("text");
                 if (t) {
-                    x = t.getAttribute("x");
-                    y = t.getAttribute("y");
-                    w = t.getBBox().width;
+                    x = parseInt(t.getAttribute("x"));
+                    y = parseInt(t.getAttribute("y"));
+                    const bb = t.getBBox();
+                    w = bb.width;
+                    h = bb.height;
                 }
-                // distingi inter stacioj kaj vojmontriloj
-                if (url == '#nun' || url == '#dekstren' || url == '#maldekstren' || url == '#vojmontrilo' ) {
+                // vojmontriloj iom manipulu por poste pli facile trakti ilin
+                if (url == '#nun' || url == '#dekstren' || url == '#maldekstren' || url == '#rondvojo' ) {
                     // memoru la detalojn de la nodo
-                    // vojmontrilojn ŝovu al def
+                    // vojmontrilojn ŝovu al def, foriginte <a>
                     if (url != '#nun') {
                         // ni ne plu bezonas la (unuan) text-elementon ene, ĉar ni anstataŭigis ĝin
                         // per unuopaj en g/use...
                         t.remove();
+                        // ni ankaŭ forigu <a> por eviti mistraktojn de klakoj
+                        n.append(...a.children); a.remove();
                         // ŝovu al defs
                         defs.append(n);
+                    } else {
+                        n.classList.add("vm_nuna");
+                        a.id = "vm_nun";
                     }
-                    this.nodoj[id] = {tip: 'montrilo', url: url, x: x, y: y, w: w};
-                } else if (url != '#nun') {
+                    // memoru parton de la geometrio ncesan por posta
+                    // aranĝo sur la stango
+                    this.nodoj[id] = {tip: 'montrilo', url: url, x: x, y: y, w: w, h: h};
+                // staciojn de la mapo memoru kun surskribo
+                } else {
                     // memoru la nodon kun url, teksto por posta pli facila traktado
                     for (const t of n.querySelectorAll("text")) {
                         text.push(t.textContent);
@@ -78,18 +91,26 @@ class YedMap {
             }
         }
 
+        // permesu fokuson
+        //this.svg.setAttribute("tabeindex","0");
+        // anst. donu al unuopaj nodoj (<a>) specifan numeron - plej bone al rondvojo-nodoj (?)
+        // vd. https://wiki.selfhtml.org/wiki/SVG/Attribute/tabindex
+
         // reagoj al klakoj sur nodo
         for (const a of this.svg.querySelectorAll("a")) {
-            a.addEventListener("click",(event) => {
-                event.preventDefault();               
-                const href = event.currentTarget.getAttributeNS(xlink, 'href') 
-                    || event.currentTarget.getAttribute("href");
-                this.iru_al_url(href);
-            })
+            a.addEventListener("click",(event) => this.iru_evento(event));
         }
 
         // iru al komenco de la migrado
         this.iru_al_url(start_url);
+    }
+
+    iru_evento(event) {
+        event.preventDefault(); 
+        const trg = event.currentTarget;                     
+        const href = trg.getAttributeNS(xlink, 'href') 
+            || trg.getAttribute("href");
+        this.iru_al_url(href,trg);
     }
 
     url_nodo(url) {
@@ -105,13 +126,19 @@ class YedMap {
         return this.nodoj[id];
     }
 
-    metu_tekston_url(url,teksto) {
+    metu_tekston_url(url,info) {
         const id = this.url_nodo(url);
         if (id) {
             const n = document.getElementById(id);
             const tn = n.querySelector("text");
             if (tn) {
-                tn.textContent = teksto.join('\n');
+                tn.textContent = info.teksto.join('\n');
+            }
+            // la tabulo kun la nuna stacio sur la 
+            // vojmontrilo ankaŭ havu aktualan url-on
+            if (url == '#nun') {
+                const a = n.querySelector("a");
+                if (a) a.setAttributeNS(xlink,"href",info.url);
             }
         }
     }
@@ -127,18 +154,24 @@ class YedMap {
         const nun = this.nodoj[nodo_id];
         console.log('@'+JSON.stringify(nun));
 
-        this.metu_tekston_url('#nun',nun.teksto);
+        this.metu_tekston_url('#nun',nun);
 
         // ni supozas nodo_id = y.node.NN
         const idP = nodo_id.split('.');
         if (idP[1] == 'node') {
 
+            // adaptu klason de la stacioj por montri la pozicion en la mapo
+            const antaua = document.querySelector(".nuna");
+            if (antaua) antaua.classList.remove("nuna");
+            document.getElementById(nodo_id).classList.add("nuna");
+
             const nn = "n"+idP[2];
             // por ĉiuj eĝoj elirantaj de la nuna nodo, ni
             // trovu la celon
-            this.vm_malplenigu();
+            this.vm_malplenigu(); // forigi malnovajn vojmontrilojn
             let vm_n = 0;
 
+            // krei novajn vojmontrilojn
             for (const e of Object.values(this.eghoj)) {
                 if (e[0] == nn) {
                     const celnodo = "y.node."+e[1].substring(1);
@@ -151,9 +184,13 @@ class YedMap {
         }
     }
 
-    iru_al_url(url) {
-        if (je_stacio) je_stacio(url);
+    /**
+     * @param url - la URL-o al kiu iri (t.e. la URL-o de la stacio, kutime komenciĝanta per #
+     * @param t_id - la HTML-id de la klakita elemento (<a>), ni povas uzi ĝin por reagi depende cu stacio aŭ vojmontrilo ks.
+     */
+    iru_al_url(url,target) {
         const n = this.url_nodo(url);
+        if (je_stacio) je_stacio(url,target);
         this.iru_al(n);
     }
 
@@ -164,11 +201,22 @@ class YedMap {
     }
 
     sur_rondvojo(nun,celo) {
+        // ĉar rondvojo povu enhavi subciklojn (bantojn) ni
+        // ĉiam provu unue iri antaŭen...
         for (let i=this.rv_pos; i<this.rondvojo.length-1; i++) {
             if (nun.url == this.rondvojo[i] && celo.url == this.rondvojo[i+1]) {
                 // aktualigu la pozicion
                 this.rv_pos = (i == this.rondvojo.length-2 
                     && this.rondvojo[0] == this.rondvojo[this.rondvojo.length-1])? 0 : i;
+                return true;
+            }
+        }
+        // por permesi ankaŭ arbitrajn saltojn, se antaŭen ne funkcia, ni
+        // provu trovi la nuna pozicion sur la rondvojo en la jam pasinta vojparto
+        for (let i=0; i<this.rv_pos; i++) {
+            if (nun.url == this.rondvojo[i] && celo.url == this.rondvojo[i+1]) {
+                // aktualigu la pozicion
+                this.rv_pos = i;
                 return true;
             }
         }
@@ -190,6 +238,10 @@ class YedMap {
             //this.metu_tekston_url(vm_url,teksto);
             const info = this.nodoj[vm_id];
             const ŝovo = pos.y0 + vm_n*pos.dy - info.y;
+
+            // ni bezonas la originan transform matrix
+            const vm_g = document.getElementById(vm_id).querySelector("g");
+            const vm_tf = vm_g.getAttribute("transform");
             // console.log(`n: ${vm_n} y0: ${y0} dy: ${dy} y: ${info.y} ŝ: ${ŝovo}`);
 
             // kreu vojmontrilon kiel g-elemento kun use- (referencante al defs) kaj a-elementoj
@@ -202,7 +254,6 @@ class YedMap {
             if (ŝovo) tf += ` translate(0 ${ŝovo})`;
             console.log("tf: "+tf);
             this.svg_attr(vm, {
-                "class": "vm",
                 "text-rendering": "geometricPrecision",
                 "font-family": "sans-serif",
                 "transform": tf
@@ -211,25 +262,42 @@ class YedMap {
             const a = document.createElementNS(ns,"a");
             const t = document.createElementNS(ns,"text");
             this.svg_attr(a, {
+                "class": "vm",
                 href: celo.url
             });
 
             // ĉar novkreitaj ni devas realdoni la klak-reagon
-            a.addEventListener("click",(event) => {
-                event.preventDefault();               
-                const href = event.currentTarget.getAttributeNS(xlink, 'href') 
-                    || event.currentTarget.getAttribute("href");
-                this.iru_al_url(href);
-            })
+            a.addEventListener("click",(event) =>  this.iru_evento(event));
+
             // la surskribo   
             this.svg_attr(t, {
                     x: info.x,
                     y: info.y,
-                    transform: "matrix(1,0,0,1,58,-32)"
+                    stroke: "none",
+                    transform: vm_tf
                 });
-            t.textContent = celo.teksto;
+            if (celo.teksto.length == 1) {
+                // unulinia teksto
+                t.textContent = celo.teksto;
+            } else {
+                // plurlinia teksto per tspan...
+                let dy = -info.h/2;
+                for (const t1 of celo.teksto) {
+                    const tsp = document.createElementNS(ns,"tspan");
+                    tsp.textContent = t1;
+                    this.svg_attr(tsp, {
+                        x: info.x,
+                        dy: dy
+                    });
+                    t.append(tsp);
+                    dy += 1.5 * info.h;
+                }
+            }
             u.setAttribute("href","#"+vm_id);
-            a.append(t); vm.append(u,a); g.append(vm);
+            // ni ĉirkaŭas la vojmontrilon (vm) per <a> por
+            // havi pli grandan areon por alklaki"
+            vm.append(u,t); a.append(vm); g.append(a);
+            //a.append(t); vm.append(u,a); g.append(vm);
         }
     }
 
@@ -246,7 +314,7 @@ class YedMap {
         const yd = this.url_nodo_info('#dekstren').y;
         const ym = this.url_nodo_info('#maldekstren').y;
         const pos = {
-            dy: Math.abs(ym - yd),
+            dy: 1.2 * Math.abs(ym - yd), // iom neakurate orientiĝante ĉe la teksto anst. la ŝildorando!
             y0: Math.min(ym,yd),
             arbitra: Math.sin(vm_n+nn) // -1..1
                // uzante Math.sin(vm_n+nn) kiel pseŭdo-arbitran nombron
