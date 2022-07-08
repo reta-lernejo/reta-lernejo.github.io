6ecfc49babc51ad045c5d5d12d103ce9600bb1ee
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-07-08 16:20:49 +0200

    SVG aparte, detailoj pri biologia sulfuruzoj

---
+++ _biokemio/PAPS.md
@@ -0,0 +1,16 @@
+---
+layout: laborfolio
+title: PAPS
+---
+
+<!-- https://chemapps.stolaf.edu/jmol/docs/ 
+https://chemapps.stolaf.edu/jmol/docs/#connect
+http://openmopac.net/Manual/Individual%20JSmol%20paths/Animations.html
+http://molmovdb.org/
+
+-->
+
+La adenozinkombinaĵo 3′-fosfoadenozin-5′-fosfosulfato kun du fosfat- kaj unu sulfat-grupoj ludas gravan rolon en
+la sintezo de karageno kaj heparino aŭ la malvenenigo de damaĝaj substancoj. Tial PAPS ekzemple troviĝas abunde en la hepato de bestoj.
+
+<script type="text/javascript" src="https://chemapps.stolaf.edu/jmol/jmol.php?model=C1%3DNC2%3DC%28C%28%3DN1%29N%29N%3DCN2%5BC%40H%5D3%5BC%40%40H%5D%28%5BC%40%40H%5D%28%5BC%40H%5D%28O3%29COP%28%3DO%29%28O%29OS%28%3DO%29%28%3DO%29O%29OP%28%3DO%29%28O%29O%29O&inline&height=600&width=800"></script>
---
+++ assets/js/sekcio-0b.js
@@ -0,0 +1,66 @@
+/**
+ * subtenas faledeblajn sekciojn en laborfiloj
+ * marku ilin per klaso sub la titoloj
+ * {: .sekcio}
+ * kaj voku tiel:
+ *   window.onload = () => {
+ *      Sekcio.aranĝo()
+ *      // ... aliaj faraĵoj en via laborfolioj
+ *   }
+ */
+
+class Sekcio {
+    static aranĝo() {
+        for (const s_titolo of document.querySelectorAll(".sekcio")) {
+            const titolo = document.createTextNode(s_titolo.textContent);
+            const details = document.createElement("details");
+            details.id = s_titolo.id
+            details.classList.add("sekcio")
+            const summary = document.createElement("summary");
+            summary.setAttribute("markdown","span");
+            summary.append(titolo);
+            details.append(summary);
+
+            // aldonu ĉiujn sekvantajn p-elementojn
+            let sekva = s_titolo.nextElementSibling;
+
+            while (sekva 
+                && !sekva.classList.contains("sekcio")
+                && sekva.tagName.toLowerCase() != s_titolo.tagName.toLowerCase()) // permesu ekz-e h3 sub h2.sekcio!
+            {                
+                details.append(sekva); // tio ankaŭ forigos ilin el la dokumento...
+                sekva = s_titolo.nextElementSibling;
+            }
+
+            // anstataŭigu  malnovan titolon per la nova sekcio
+            s_titolo.parentElement.replaceChild(details,s_titolo);
+        }
+
+        // kiam ni klakas ligon al unu el la sekcioj, la celata sekcio devos malfermiĝi
+        // Ni limigas al "p a", por eviti "svg a"-elementojn, bedaŭrinde
+        // querySelector ne subtenas elekton de nomspaco per sintakso "html|a"
+        for (const a of document.querySelectorAll("p a")) {
+            a.addEventListener("click", (event) => {
+                const ct = event.currentTarget;
+                const href = ct.getAttribute("href");
+                if (href[0] == '#')
+                    Sekcio.malfermu(href.substring(1));
+            });
+        }
+        
+    }
+
+    static malfermu(s_id,fermu_aliajn) {
+        const sekcio = document.getElementById(s_id);
+        for (const d of document.querySelectorAll(".sekcio")) {
+          //  malfermu la celitan...
+          if (d.id == s_id) {
+              d.setAttribute("open","open");
+          } else if (fermu_aliajn) {
+            // fermu aliajn sekciojn 
+              d.removeAttribute("open");
+          }
+        }
+    }
+}
+
