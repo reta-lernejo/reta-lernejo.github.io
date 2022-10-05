6a6ef6f41774028a6a3b935075c6f802fb4aa307
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-10-05 17:06:06 +0200

    laboratorio.js, eksperimento precipito...

--- _kemio/precipito.md
+++ _kemio/precipito.md
@@ -1,4 +1,3 @@
-
 ---
 layout: laborfolio
 title: Precipito
@@ -6,6 +5,7 @@ js:
   - folio-0b
   - sekcio-0b 
   - mathjax/es5/tex-chtml
+  - laboratorio-0a
 ---
 
 
@@ -16,3 +16,53 @@ https://en.wikipedia.org/wiki/Solubility#Solubility_of_ionic_compounds_in_water
 https://de.wikipedia.org/wiki/L%C3%B6slichkeit
 https://www.hoffmeister.it/chemie/14-ionen-salze-faellungsreaktionen_und_ionenbindung.pdf
 -->
+
+<script>
+  let lab;
+
+  lanĉe(()=>{
+    lab = new Laboratorio(ĝi("#eksperimento"));
+    const glaso = Lab.glaso();
+    const NaCl = Lab.gutbotelo("NaCl",70);
+    const AgNO3 = Lab.gutbotelo("AgNO₃",15);
+    lab.metu(glaso,200,500);
+    lab.metu(NaCl,50,500);
+    lab.metu(AgNO3,350,500);
+  })
+</script>
+
+<svg id="eksperimento"
+    version="1.1" 
+    xmlns="http://www.w3.org/2000/svg" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" viewBox="-10 -10 500 800">
+ <style type="text/css">
+    <![CDATA[
+      .vitro {
+        fill: none;
+        stroke: black;
+        stroke-width: 1.0;
+      }
+
+      .ujo text {
+        font-size: 10px;
+      }
+
+/*
+      .gutbotelo .likvo {
+        clip-path: path('M0,-100 L0,-4 Q0,0 4,0 L36,0 Q40,0 40,-4 L40,-100 Z');
+      }  
+      */    
+
+      .likvo {
+        fill: silver;
+        stroke: gray;
+        stroke-width: 0.1;
+      }
+    ]]>
+  </style>
+  <defs>
+    <pattern id="strie" viewBox="0,0,4,1" height="20%" width="20%">
+      <rect width="2" height="1"/>
+    </pattern>
+  </defs>
+</svg>
---
+++ assets/js/laboratorio-0a.js
@@ -0,0 +1,226 @@
+
+class LabSVG {
+    
+    constructor(svg) {
+        this.svg = svg;
+    }
+
+    /**
+     * Trovas SVG-elementon per CSS-elektilo
+     */
+    trovu(elektilo) {
+        return this.svg.querySelector(elektilo)
+    }
+
+    /** 
+     * Ŝovas elementon uzante atributon transform al nova pozicio (dx,dy)
+     */
+    ŝovu(elm, x, y=0) {
+        if (x || y)
+            this.atr(elm, {transform: `translate(${x} ${y})`});
+    }
+
+    /**
+     * desegnu simbolon id ĉe (x,y) 
+     */
+    simbol_uzo(id,x,y) {
+        const use = this.kreu("use", {
+            href: "#"+id,
+            x: x,
+            y: y
+        });
+        this.svg.append(use);
+    }    
+
+    /**
+     * redonas elementon defs (kreante ĝins se ankoraŭ mankas)
+     */
+    difinoj() {
+        let defs = this.svg.querySelector("defs");
+        if (!defs) {
+            defs = this.desegno.kreu("defs");
+            this.svg.prepend(defs);
+        }
+        return defs;
+    }
+}
+
+class Lab {
+
+    /** Kreas SVG-elementon kun atributoj
+     * @param nomo elementnomo, ekz-e 'div'
+     * @param atributoj objekto kies kampoj estas la atributnomoj kaj ties valoroj
+     */
+    static e(nomo, atributoj, teksto) {
+        const ns = "http://www.w3.org/2000/svg";
+        const el = document.createElementNS(ns, nomo);
+        if (atributoj) {
+            for (const [atr,val] of Object.entries(atributoj)) {
+                el.setAttribute(atr,val);
+            }
+        };
+        if (teksto) el.textContent = teksto;
+        return el;
+    }
+
+    /**
+     * Aldonas aŭ ŝanĝas atributojn de SVG-DOM-elemento
+     * 
+     * @param elemento la DOM-elemento 
+     * @param atributoj objekto kies kampoj estas la atributnomoj kaj ties valoroj
+     * @returns 
+     */
+    static a(elemento, atributoj) {
+        if (atributoj) {
+            for (const [atr,val] of Object.entries(atributoj)) {
+                elemento.setAttribute(atr,val);
+            }
+        };
+        return elemento;
+    }
+
+    /** 
+     * Desegnas rondigitan rektangulon
+     * @param {number} w larĝo
+     * @param {number} h alteco
+     * @param {number} a angulgrandeco
+     * @param {number} x x-koordinato (maldekstre)
+     * @param {number} y y-koordinato (supre)
+     */
+    static rrekt(w,h,a,x=0,y=0) {
+        return Lab.e("path",{
+            d: `M${x+a},${y} L${x+w-a},${y} Q${x+w},${y} ${x+w},${y+a} ` // supra linio
+             + `L${x+w},${y+h-a} Q${x+w},${y+h} ${x+w-a},${y+h}`  // dekstra linio
+             + `L${x+a},${y+h} Q${x},${y+h} ${x},${y+h-a}`  // malsupra linio
+             + `L${x},${y+a} Q${x},${y} ${x+a},${y}` // maldekstra linio
+        });
+    }
+
+    /** 
+     * Desegnas nur malsupre rondigitan rektangulon
+     * @param {number} w larĝo
+     * @param {number} h alteco
+     * @param {number} a angulgrandeco
+     * @param {number} x x-koordinato (maldekstre)
+     * @param {number} y y-koordinato (supre)
+     */
+     static r_rekt(w,h,a,x=0,y=0) {
+        return Lab.e("path",{
+            d: `M${x},${y} L${x+w},${y} ` // supra linio
+             + `L${x+w},${y+h-a} Q${x+w},${y+h} ${x+w-a},${y+h}`  // dekstra linio
+             + `L${x+a},${y+h} Q${x},${y+h} ${x},${y+h-a}`  // malsupra linio
+             + `L${x},${y+a} Z` // maldekstra linio
+        });
+    }
+
+    /** 
+     * Desegnas rektangulon rondigitan malsupre konvekse kaj supre konkave
+     * @param {number} w larĝo
+     * @param {number} h alteco
+     * @param {number} a angulgrandeco malsupre
+     * @param {number} u angulgrandeco supre
+     * @param {number} x x-koordinato (maldekstre)
+     * @param {number} y y-koordinato (supre)
+     */
+    static rurekt(w,h,a,u,x=0,y=0) {
+        return Lab.e("path",{
+            d: `M${x},${y-u} Q${x},${y} ${x+u},${y} L${x+w-u},${y} Q${x+w},${y} ${x+w},${y-u}` // supra linio
+             + `L${x+w},${y+h-a} Q${x+w},${y+h} ${x+w-a},${y+h}`  // dekstra linio
+             + `L${x+a},${y+h} Q${x},${y+h} ${x},${y+h-a}`  // malsupra linio
+             + `L${x},${y+a} Z` // maldekstra linio
+        });
+    }
+
+
+    static glaso() {
+        const g = Lab.e("g", { class: "ujo" });
+        const ujo = Lab.e("path",{
+            d: "M-5,-300 Q0,-300 0,-295 L0,-5 Q0,0 5,0 L 95,0 Q100,0 100,-5 L100,-295 Q100,-300 105,-300 Z",
+            class: "vitro"
+        });
+        /*
+        const enhavo = Lab.e("rect", {
+            x: 0, y: -200, width: 100, height: 200,
+            class: "likvo"
+        });
+        */
+        const enhavo = Lab.rurekt(100,200,5,5,0,-200);
+        Lab.a(enhavo,{
+            class: "likvo"
+        });
+        g.append(enhavo,ujo);
+        return(g);
+    }
+
+    /**
+     * Kreas gutbotelon 
+     * @param {string} etikedo
+     * @param {number} pleno enhavpleneco 0..100
+     * @param {number} angulo angulo, je kiu la botelo estu turnita
+     * 
+     */
+    static gutbotelo(etikedo,pleno,angulo) {
+        const g = Lab.e("g", { class: "ujo gutbotelo" });
+        const bordo = "M0,-100 L0,-4 Q0,0 4,0 L36,0 Q40,0 40,-4 L40,-100 Z";
+        const ujo = Lab.e("path",{
+            d: bordo,
+            class: "vitro"
+        });
+        const kovrilo = Lab.e("polyline",{
+            points: "-2,-100 -2,-105 10,-108 18,-130 22,-130 30,-108 42,-105 42,-100 -2,-100",
+            class: "plasto"
+        });
+
+        // enhavo
+        let enhavo = '';
+        if (pleno) {
+            //enhavo = Lab.rurekt(40,pleno,5,5,0,-pleno);
+            enhavo = Lab.e("path",
+            {
+                d: `M0,0 L0,-${pleno} L100,-${pleno} L100,0 Z`,
+                //"clip-path": `path('${bordo}')`,
+                "clip-path": `path('M0,0 L0,100 L40,100 L40,0z')`,
+                //class: "likvo"
+                fill: "red"
+            })
+        }
+        // surskribo
+        const surskribo = Lab.e("text", {
+                x: 3, y: -60
+            }, etikedo
+        );
+        g.append(enhavo,ujo,kovrilo,surskribo);
+        return g;
+    }
+}
+
+
+class Laboratorio extends LabSVG {
+    constructor(svg) {
+        super(svg);
+        const ns = "http://www.w3.org/2000/svg";
+        const g = document.createElementNS(ns, 'g');
+        g.id = "lab_aranĝo";
+        this.aranĝo = g;
+        this.svg.append(g);
+    }
+
+    /** Metas novajn ilon en la aranĝon de la laboratorio,
+     * @param {object} ilo la ilo, kreita per ujo() k.s.
+     * @param {number} x x-koordinato (0=malsdesktre)
+     * @param {number} y y-koordinato (0=supre)
+     */
+    metu(ilo,x,y) {
+        const e = ilo; // aŭ svg-grupo de ilo, se ĝi ne mem estas SVG-elemento
+        const X = x||0;
+        const Y = y||0;
+        if (x || y) {
+            Lab.a(e, {
+                transform: `translate(${x} ${y})`
+            });
+        }
+        this.aranĝo.append(ilo)
+    }
+
+
+}
