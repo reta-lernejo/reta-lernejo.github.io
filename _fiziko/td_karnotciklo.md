2135432fe9f440146f01e517fe3f5d3eb0b5470e
Wolfram Diestel <diestel@steloj.de>
2023-04-16 15:17:20 +0200

    perparo kalkuloj/trakuro de Karnot-ciklo

--- _fiziko/td_karnotciklo.md
+++ _fiziko/td_karnotciklo.md
@@ -16,9 +16,19 @@ https://de.wikipedia.org/wiki/Carnot-Prozess
 
 -->
 
-<canvas id="karnot" width="400" height="400"></canvas>
-<canvas id="pV_dgr" width="400" height="400"></canvas>
-<canvas id="TS_dgr" width="400" height="400"></canvas>
+
+<style>
+    canvas {
+        border: 2px solid cornflowerblue;
+    }
+</style>
+
+<canvas id="karnot" width="300" height="400"></canvas>
+
+--
+
+<canvas id="pV_dgr" width="300" height="300"></canvas>
+<canvas id="TS_dgr" width="300" height="300"></canvas>
 
 <script>
 
@@ -32,27 +42,73 @@ const modelo = new Diagramo(karnot);
 function modelo_pentru() {
     modelo.viŝu(); // ĉu necesas ĉiufoje?
 
-    // medio
-    // koloro laŭ temperaturo...
-    modelo.rektangulo(80,20,140,380,"#c11");
-    modelo.linio(80,20,80,400);
-    modelo.linio(80,400,220,400);
-    modelo.linio(220,20,220,400);
+    function medio() {
+        // medio
+        // koloro laŭ temperaturo...
+        modelo.rektangulo(80,20,140,380,"#11c");
+        modelo.linio(80,20,80,400);
+        modelo.linio(80,400,220,400);
+        modelo.linio(220,20,220,400);
+    }
 
-    // gasujo
-    modelo.rektangulo(100,20,100,300,"#fff");
-    modelo.linio(100,20,100,320);
-    modelo.linio(100,320,200,320);
-    modelo.linio(200,20,200,320);
+    function gasujo() {
+        // ciklo-ŝaltilo
+        function nazo(alto) {
+            modelo.linio(100,alto-2,104,alto);
+            modelo.linio(100,alto+2,104,alto);
+            modelo.linio(200,alto-2,196,alto);
+            modelo.linio(200,alto+2,196,alto);
+        }
 
-    // piŝto
-    //modelo.linio(101,200,199,200,"#bbb",10);
-    modelo.rektangulo_h3k(101,200,98,10,"#eee","#bbb","#999");
-    modelo.rektangulo_h3k(120,200-80,60,80,"#eee","#bbb","#999");
+        // gasujo
+        modelo.rektangulo(100,20,100,300,"#fff");
+        modelo.linio(100,20,100,320);
+        modelo.linio(100,320,200,320);
+        modelo.linio(200,20,200,320);
 
-}    
+        // altec-markoj por avanci en la ciklo
+        // nazo(40); - ne necesas, ĉar la temperaturo difinas la supran punkton
+        nazo(140);
+        // nazo(240); - ne necesas, ĉar la temperaturo difinas la malsupran punkton
+    }
 
+    function piŝto() {
+        //modelo.linio(101,200,199,200,"#bbb",10);
+        modelo.rektangulo_h3k(101,200,98,10,"#eee","#bbb","#999");
+        modelo.rektangulo_h3k(120,200-80,60,80,"#eee","#bbb","#999");
+    }
+
+    medio();
+    gasujo();
+    piŝto()
+}
 
 modelo_pentru();
 
 </script>
+
+## paŝo 1 - temperaturkonserva (izoterma) kunpremiĝo
+
+Dum la temperaturo estas teanata konstante malalta la gaso kunpremiĝas per ekstera premo, t.e. laboro, aplikata al la gaso.
+La varmo estiĝanta en la gaso per kunpremiĝo estas transdonata al la ekstera malvarma medio. Pro la konstanta temperaturo
+la produkto $$pV$$ restas konstanta, do pro la malgrandiĝanta volumeno la premo kreskas.
+
+Ĉar temas pri ideala gaso, konstanta temperaturo signifas konstanta interna energio $$dU = 0$$. Konsekvence la tuta farita laboro
+(la surfaco sub la kurbo 1-2 en la p-V-diagramo) transformiĝos al varmo akceptata de la malvarma medio, egala al la surfaco 
+sub la kurbo 1-2 en la T-S-diagramo.
+
+## paŝo 2 - varmkonserva (adiabata) kunpremiĝo
+
+La piŝtujo estas varmizolita dum la piŝto plu kunpremas la gason, kies temperaturo pro tio altiĝas. Pro la izolo da tuta farita laboro
+(la surfaco sub la kurbo 2-3 en la p-V-diagramo) altigas la internan energion de la gaso.
+
+## paŝo 3 - temperaturkonserva (izoterma) etendiĝo
+
+Ĉe temperaturo tenata konstante alta, la gaso etendiĝas pro interna premo. Pro la konstanta temperaturo la interna energio same
+restas konstanta kaj la tuta farita laboro (la surfaco sub la kurbo 3-4 en la p-V-diagramo) estas
+egala al la varmo (la surfaco sub la kurbo 3-4 en la T-S-diagramo) transdonita de la alttemperatura ekstera medio al la gaso en la piŝtujo.
+Pro la konstanta temperaturo ankaŭ la produko $$pV$$ restas konstnat. Konsekvence la premo malaltiĝas inverse proporcie al la kreskanta volumeno.
+
+## paŝo 4 - varmkonserva (adiabata) etendiĝo
+
+La piŝtujo estas varmizolita la gaso plu etendiĝas, kies temperaturo kaj premo pro tio malaltiĝas. Pro la izolo, la laboro farita de la gaso sammezure malaltigas la internan energion.
--- assets/js/f_karnotciklo-0a.js
+++ assets/js/f_karnotciklo-0a.js
@@ -1,45 +1,123 @@
 /**
  * Simulas ciklon de Karnoto (laŭ Nicolas Léonard Sadi Carnot) kun ideala gaso.
- * La modelo konsistas el la gaso en la piŝto kaj la ekstera medio. Por tiuj ni uzas apartan klason, tiel
- * principe oni povus anstataŭigi la idealan gason per iu alia.
+ * La modelo konsistas el la gaso en la piŝtujo kaj la ekstera medio. 
+ * Por la gaso ni uzas apartan klason, tiel principe oni povus anstataŭigi la idealan gason per iu alia.
  * 
  * La Karnot-ciklo uzas tri diversajn mediojn: varman, malvarman kaj varmizolitan.
+ * 
+ * Ni provizore uzas 1 molon da ideala gaso, kiu havas 24l ĉe normpremo/20°C (22,4l ĉe 73.15K)
+ * kaj 48l ĉe 600°C. 
  */
 
 
 
 class KCGaso {
+    static norm_p = 1e5; // normpremo estas 1000 hPa
+    static norm_T = 293.15; // normtemperaturo en K
+    static R = 8.31446261815324; // universala gaskonstanto en J/K/mol
+    static kappa = 5/3; // adiabata koeficiento
+
+    /**
+     * Kalkulas la volumenon depende de temperaturo, molkvanto kaj premo
+     * el la ekvacio por ideala gaso
+     * @param {*} temperaturo 
+     * @param {*} premo 
+     * @param {*} moloj 
+     */
+    static volumeno(temperaturo,premo,moloj=1) {
+        return moloj*KCGaso.R*temperaturo / premo;
+    }
+
     /**
      * KReas volumenon da ideala gaso. Se ne alie donita ni supozas heliumon (atommaso u=4).
-     * @param {*} volumeno 
      * @param {*} temperaturo 
+     * @param {*} volumeno 
      * @param {*} premo 
      * @param {*} atommaso 
      */
-    constructor(volumeno,temperaturo=KCMedio.norm_T,premo=KCMedio.norm_p,atommaso=4) {
-        this.volumeno = volumeno;
+    constructor(temperaturo=KCGaso.norm_T,volumeno) {
         this.temperaturo = temperaturo;
-        this.premo = premo;
-        this.umaso = atommaso;
+        this.volumeno = volumeno || KCGaso.volumeno(this.temperaturo,KCGaso.norm_p);
     }
 
-}
-
-class KCMedio {
-    static norm_p = 1e5; // normpremo estas 1000 hPa
-    static norm_T = 293.15; // normtemperaturo en K
-
-    constructor(temperaturo=KCMedio.norm_T, premo=KCMedio.norm_p) {
-        this.temperaturo = temperaturo;
-        this.premo = premo;
+    /**
+     * Ŝanĝas la volumenon de la gaso izoterme, t.e. kun varminterŝanĝo kun la medio
+     * tiel konservanta la internan energion
+     * @param {*} dV 
+     */
+    dV_izoterma(dV) {
+        this.volumeno += dV;
     }
+
+    /**
+     * Ŝanĝas ka volumenon adiabate, t.e. sen varminterŝanĝo kun la ekstero
+     * vd. https://de.wikipedia.org/wiki/Adiabatische_Zustands%C3%A4nderung#Adiabaten_des_idealen_Gases
+     * @param {*} dV 
+     */
+    dV_adiabata(dV) {
+        this.tempteraturo = this.temperaturo * Math.pow((this.volumeno/(this.volumeno+dV)),KCGaso.kappa-1);
+        this.volumeno += dV;
+    }
+
+    /**
+     * Redonas la premon de la gaso, kalkulitan el ĝia temperaturo kaj volumeno
+     */
+    premo() {
+        return KCGaso.R * this.temperaturo / this.volumeno;
+    }
+
 }
 
 class KCiklo {
-    constructor(gaso,T_alta,T_malalta) {
+    // Tk: temperaturkonserva, Qk: varmkonserva (adiabata)
+    // V+ etendiĝo, V- kunpremiĝo
+    static paŝoj = ["Tk_V-","Qk_V-","Tk_V+","Qk_V+"];
+    static igV = 0.0224; // mola volumeno de ideala gaso = 22,4l
+    static dV = 0.001; // 1 litro
+
+    constructor(gaso,T_alta,T_malalta,V12=igV/2,V34=igV) {
         this.gaso = gaso || new KCGaso(T_malalta);
-        this.varma = new KCMedio(T_alta);
-        this.malvarma = new KCMedio(T_malalta);
+        this.T_alta = T_alta;
+        this.T_malalta = T_malalta;
         //this.izolita = ???
+        this.paŝo = KCiklo.paŝoj[0];
+
+        this.V12 = V12; // volumeno, kie T-konserva kunpremo transiru al Q-konserva
+        this.V34 = V34; // volumeno, kie T-konserva entediĝo transiru al Q-konserva
+    }
+
+    /**
+     * Iteracias tra la ciklo
+     */
+    iteracio() {
+        switch (this.paŝo) {
+        case "Tk_V-": 
+            if (this.gaso.volumeno > this.V12) {
+                this.gaso.dV_izoterma(-KCiklo.dV);
+                break;
+            }
+            this.paŝo = "Qk_V-";
+
+        case "Qk_V-":
+            if (this.gaso.temperaturo < this.T_alta) {
+                this.gaso.dV_adiabata(-KCiklo.dV);
+                break;                    
+            }
+            this.paŝo = "Tk_V+"; // PLIBONIGU: se la temperaturo tro devias, ni eble devus alĝustigi...?
+
+        case "Tk_V+": 
+            if (this.gaso.volumeno < this.V34) {
+                this.gaso.dV_izoterma(KCiklo.dV);
+                break;
+            }
+            this.paŝo = "Qk_V+";     
+
+        case "Qk_V+":
+            if (this.gaso.temperaturo > this.T_malalta) {
+                this.gaso.dV_adiabata(KCiklo.dV);
+                break;                    
+            }
+            this.paŝo = "Tk_V-"; // PLIBONIGU: se la temperaturo tro devias, ni eble devus alĝustigi...?                       
+        }
     }
 }
